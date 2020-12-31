8365bb574f59a4d8f18054082edec375c0ad9b38
Wahyu Amirulloh <amirullohw@gmail.com>
2020-12-31 14:52:37 +0700

    developing frontpage for post product list

--- front-page.php
+++ front-page.php
@@ -1,26 +1,42 @@
-<?php get_header();
-
-?>
-<div class="title">
-	<h2>Produk<hr/></h2>
+<?php get_header(); ?>
+<div class="main-content container-fluid">
+    <div class="page-title">
+        <div class="row">
+            <div class="col-12 col-md-6">
+                <h3>Produk</h3>
+                <p class="text-subtitle text-muted">Dapatkan produk terbaru dan terlaris kami disini</p>
+            </div>
+        </div>
+    </div>
+    <?php if(have_posts()){
+    	while(have_posts()){
+    		the_post(); ?>
+    <div class="card col-xl-4 col-md-6 col-sm-12 h-500">
+        <div class="card-content">
+            <div class="card-body">
+                <h4 class="card-title"><?php the_title(); ?></h4>
+                <p class="card-text">
+                    <?php the_excerpt(); ?>
+                </p>
+            </div>
+            <div class="card-image">
+	            <?php 
+	            if(has_post_thumbnail()){
+	            	the_post_thumbnail('medium');
+	            } else {
+	            	?>
+	            	<svg class="bd-placeholder-img card-img-top" width="100%" height="200" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image cap" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text></svg>
+	            	<?php
+	            }
+	             ?>
+	        </div>
+        </div>
+        <div class="card-footer d-flex justify-content-between">
+            <span><?php echo 'Rp' . number_format(get_field('price'), 0, ".", ".") ?></span>
+            <button class="btn btn-success">Buy Now</button>
+        </div>
+    </div>
+    <?php }
+    } ?>
 </div>
-<?php
-if (have_posts()) {
-	while (have_posts()) {
-		the_post();
-		?>
-		<div class="card">
-			<div class="product">
-				<?php echo the_post_thumbnail('medium') ?>
-				<h4><?php echo the_title() ?></h4>
-				<p><?php echo the_excerpt() ?></p>
-			</div>
-			<div class="product-order">
-				<button>Buy Now</button>
-			</div>
-		</div>
-		<?php
-	}
-}
-
-get_footer(); ?>
+<?php get_footer(); ?>
--- functions.php
+++ functions.php
@@ -2,8 +2,9 @@
 $version = wp_get_theme()->get('version');
 
 function wp_register_styles() {
+	wp_enqueue_style('bantumkm_bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css', $version);
 	wp_enqueue_style('bantumkm_style', get_template_directory_uri() . '/style.css', $version);
-	wp_enqueue_style('bantumkm_font', 'https://fonts.googleapis.com/css2?family=Raleway&display=swap');
+	// wp_enqueue_style('bantumkm_font', 'https://fonts.googleapis.com/css2?family=Raleway&display=swap');
 }
 function wp_register_scripts() {
 	wp_enqueue_script('bantumkm_script', get_template_directory_uri() . '/assets/js/script.js', $version);
@@ -17,5 +18,9 @@ function wp_theme_support() {
 add_action('wp_enqueue_scripts', 'wp_register_styles');
 add_action('wp_enqueue_scripts', 'wp_register_scripts');
 add_action('after_setup_theme', 'wp_theme_support');
+
+add_filter('excerpt_length', function(){
+	return 15;
+});
 
 ?>
