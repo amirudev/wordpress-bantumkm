4b48e24bc3c621919a604aae870ef594c48b1069
Wahyu Amirulloh <amirullohw@gmail.com>
2021-01-02 17:25:43 +0700

    acf_add_local_field_group

--- functions.php
+++ functions.php
@@ -15,10 +15,66 @@ function wp_theme_support() {
 	add_theme_support('post-thumbnails');
 }
 
+// Custom Post Type
+function wp_custom_post_type() {
+	$support = array(
+		'title',
+		'title',
+		'editor',
+		'thumbnail',
+		'custom-fields'
+	);
+	$labels = array(
+		'name' => _x('Produk', 'plural'),
+		'singular_name' => _x('Produk', 'singular'),
+		'add_new' => _x('Tambah Produk', 'add new')
+	);
+	$args = array(
+		'supports' => $support,
+		'labels' => $labels,
+		'public' => true,
+		'query_var' => true,
+		'rewrite' => array('slug' => 'Product'),
+		'has_archive' => true,
+		'hierarchical' => true
+	);
+	register_post_type('Products', $args);
+}
+
+// ACF Add Local Fields
+if( function_exists('acf_add_local_field_group') ){
+	acf_add_local_field_group(
+		array(
+			'key' => 'price_group',
+			'title' => 'Price',
+			'fields' => array(
+				array(
+					'key' => 'price_field',
+					'label' => 'Price',
+					'name' => 'Price',
+					'type' => 'number'
+				)
+			),
+			'location' => array(
+				array(
+					array(
+						'param' => 'post_type',
+						'operator' => '==',
+						'value' => 'product' // Not working for custom post type 'product'
+					),
+				),
+			),
+		)
+	);
+}
+
 // Customize website functionality
 add_action('wp_enqueue_scripts', 'wp_register_styles');
 add_action('wp_enqueue_scripts', 'wp_register_scripts');
 add_action('after_setup_theme', 'wp_theme_support');
+
+// Registering post type
+add_action('init', 'wp_custom_post_type');
 
 // Excerpt Configuration
 add_filter('excerpt_length',function (){
