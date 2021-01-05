3516a95ea0b1bbfa6bd1aed9a08bfb41807e5e57
Wahyu Amirulloh <amirullohw@gmail.com>
2021-01-05 10:53:51 +0700

    skeletoning front-page

--- front-page.php
+++ front-page.php
@@ -1,3 +1,30 @@
 <?php get_header(); ?>
-
-<?php get_footer(); ?>
+    <div class="grid-container product-container">
+		<div class="main-product">
+			<div class="card">
+				<div class="card-body">
+					<h5 class="card-title">Special title treatment</h5>
+					<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+					<a href="#" class="btn btn-primary">Go somewhere</a>
+				</div>
+			</div>
+		</div>
+		<div class="side-product">
+			<div class="card">
+				<div class="card-body">
+					<h5 class="card-title">Special title treatment</h5>
+					<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+					<a href="#" class="btn btn-primary">Go somewhere</a>
+				</div>
+			</div>
+		</div>
+		<div class="side-product">
+			<div class="card">
+				<div class="card-body">
+					<h5 class="card-title">Special title treatment</h5>
+					<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+					<a href="#" class="btn btn-primary">Go somewhere</a>
+				</div>
+			</div>
+		</div>
+    </div>
