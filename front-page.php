8c5d553f590f52dd07d13b813b5164740aacaaba
Wahyu Amirulloh <amirullohw@gmail.com>
2021-01-05 14:36:40 +0700

    frontpage skeletoning

--- front-page.php
+++ front-page.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
     <div class="grid-container product-container">
 		<div class="main-product">
-			<div class="card">
+			<div class="card" id="card-1">
 				<div class="card-body">
 					<h5 class="card-title">Special title treatment</h5>
 					<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
@@ -10,7 +10,7 @@
 			</div>
 		</div>
 		<div class="side-product">
-			<div class="card">
+			<div class="card" id="card-2">
 				<div class="card-body">
 					<h5 class="card-title">Special title treatment</h5>
 					<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
@@ -19,7 +19,7 @@
 			</div>
 		</div>
 		<div class="side-product">
-			<div class="card">
+			<div class="card" id="card-3">
 				<div class="card-body">
 					<h5 class="card-title">Special title treatment</h5>
 					<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
@@ -27,4 +27,299 @@
 				</div>
 			</div>
 		</div>
-    </div>
+	</div>
+	<section class="card-container">
+		<div class="page-title">
+			<div class="row">
+				<div class="col-12 col-md-6">
+					<h3>Blog</h3>
+					<p class="text-subtitle text-muted">Bootstrap’s cards provide a flexible and extensible content container with multiple variants and options.</p>
+				</div>
+			</div>
+		</div>
+        <div class="row">
+            <div class="col-xl-4 col-md-6 col-sm-12">
+                <div class="card">
+                    <div class="card-content">
+                        <div class="card-body">
+                            <h4 class="card-title">Card With Header And Footer</h4>
+                            <p class="card-text">
+                                Gummies bonbon apple pie fruitcake icing biscuit apple pie jelly-o sweet roll. Toffee
+                                sugar plum sugar plum jelly-o jujubes bonbon dessert carrot cake.
+                            </p>
+                        </div>
+                        <!-- Image placeholder -->
+                    </div>
+                    <div class="card-footer d-flex justify-content-between">
+                        <span>Rp100.000</span>
+                        <button class="btn btn-success">Beli Sekarang</button>
+                    </div>
+				</div>
+				<div class="card">
+                    <div class="card-content">
+                        <div class="card-body">
+                            <h4 class="card-title">Card With Header And Footer</h4>
+                            <p class="card-text">
+                                Gummies bonbon apple pie fruitcake icing biscuit apple pie jelly-o sweet roll. Toffee
+                                sugar plum sugar plum jelly-o jujubes bonbon dessert carrot cake.
+                            </p>
+                        </div>
+                        <!-- Image placeholder -->
+                    </div>
+                    <div class="card-footer d-flex justify-content-between">
+                        <span>Rp100.000</span>
+                        <button class="btn btn-success">Beli Sekarang</button>
+                    </div>
+				</div>
+				<div class="card">
+                    <div class="card-content">
+                        <div class="card-body">
+                            <h4 class="card-title">Card With Header And Footer</h4>
+                            <p class="card-text">
+                                Gummies bonbon apple pie fruitcake icing biscuit apple pie jelly-o sweet roll. Toffee
+                                sugar plum sugar plum jelly-o jujubes bonbon dessert carrot cake.
+                            </p>
+                        </div>
+                        <!-- Image placeholder -->
+                    </div>
+                    <div class="card-footer d-flex justify-content-between">
+                        <span>Rp100.000</span>
+                        <button class="btn btn-success">Beli Sekarang</button>
+                    </div>
+				</div>
+			</div>
+			<div class="col-xl-4 col-md-6 col-sm-12">
+                <div class="card">
+                    <div class="card-content">
+                        <div class="card-body">
+                            <h4 class="card-title">Card With Header And Footer</h4>
+                            <p class="card-text">
+                                Gummies bonbon apple pie fruitcake icing biscuit apple pie jelly-o sweet roll. Toffee
+                                sugar plum sugar plum jelly-o jujubes bonbon dessert carrot cake.
+                            </p>
+                        </div>
+                        <!-- Image placeholder -->
+                    </div>
+                    <div class="card-footer d-flex justify-content-between">
+                        <span>Rp100.000</span>
+                        <button class="btn btn-success">Beli Sekarang</button>
+                    </div>
+				</div>
+				<div class="card">
+                    <div class="card-content">
+                        <div class="card-body">
+                            <h4 class="card-title">Card With Header And Footer</h4>
+                            <p class="card-text">
+                                Gummies bonbon apple pie fruitcake icing biscuit apple pie jelly-o sweet roll. Toffee
+                                sugar plum sugar plum jelly-o jujubes bonbon dessert carrot cake.
+                            </p>
+                        </div>
+                        <!-- Image placeholder -->
+                    </div>
+                    <div class="card-footer d-flex justify-content-between">
+                        <span>Rp100.000</span>
+                        <button class="btn btn-success">Beli Sekarang</button>
+                    </div>
+				</div>
+				<div class="card">
+                    <div class="card-content">
+                        <div class="card-body">
+                            <h4 class="card-title">Card With Header And Footer</h4>
+                            <p class="card-text">
+                                Gummies bonbon apple pie fruitcake icing biscuit apple pie jelly-o sweet roll. Toffee
+                                sugar plum sugar plum jelly-o jujubes bonbon dessert carrot cake.
+                            </p>
+                        </div>
+                        <!-- Image placeholder -->
+                    </div>
+                    <div class="card-footer d-flex justify-content-between">
+                        <span>Rp100.000</span>
+                        <button class="btn btn-success">Beli Sekarang</button>
+                    </div>
+				</div>
+			</div>
+			<div class="col-xl-4 col-md-6 col-sm-12">
+                <div class="card">
+                    <div class="card-content">
+                        <div class="card-body">
+                            <h4 class="card-title">Card With Header And Footer</h4>
+                            <p class="card-text">
+                                Gummies bonbon apple pie fruitcake icing biscuit apple pie jelly-o sweet roll. Toffee
+                                sugar plum sugar plum jelly-o jujubes bonbon dessert carrot cake.
+                            </p>
+                        </div>
+                        <!-- Image placeholder -->
+                    </div>
+                    <div class="card-footer d-flex justify-content-between">
+                        <span>Rp100.000</span>
+                        <button class="btn btn-success">Beli Sekarang</button>
+                    </div>
+				</div>
+				<div class="card">
+                    <div class="card-content">
+                        <div class="card-body">
+                            <h4 class="card-title">Card With Header And Footer</h4>
+                            <p class="card-text">
+                                Gummies bonbon apple pie fruitcake icing biscuit apple pie jelly-o sweet roll. Toffee
+                                sugar plum sugar plum jelly-o jujubes bonbon dessert carrot cake.
+                            </p>
+                        </div>
+                        <!-- Image placeholder -->
+                    </div>
+                    <div class="card-footer d-flex justify-content-between">
+                        <span>Rp100.000</span>
+                        <button class="btn btn-success">Beli Sekarang</button>
+                    </div>
+				</div>
+				<div class="card">
+                    <div class="card-content">
+                        <div class="card-body">
+                            <h4 class="card-title">Card With Header And Footer</h4>
+                            <p class="card-text">
+                                Gummies bonbon apple pie fruitcake icing biscuit apple pie jelly-o sweet roll. Toffee
+                                sugar plum sugar plum jelly-o jujubes bonbon dessert carrot cake.
+                            </p>
+                        </div>
+                        <!-- Image placeholder -->
+                    </div>
+                    <div class="card-footer d-flex justify-content-between">
+                        <span>Rp100.000</span>
+                        <button class="btn btn-success">Beli Sekarang</button>
+                    </div>
+				</div>
+			</div>
+		</div>
+		<div class="page-title">
+			<div class="row">
+				<div class="col-12 col-md-6">
+					<h3>Blog</h3>
+					<p class="text-subtitle text-muted">Bootstrap’s cards provide a flexible and extensible content container with multiple variants and options.</p>
+				</div>
+			</div>
+		</div>
+        <div class="row">
+            <div class="col-xl-4 col-md-6 col-sm-12">
+                <div class="card">
+                    <div class="card-content">
+                        <!-- Image Placeholder -->
+                        <div class="card-body">
+                            <h5 class="card-title">Be Single Minded</h5>
+                            <p class="card-text">
+                                Chocolate sesame snaps apple pie danish cupcake sweet roll jujubes tiramisu.Gummies
+                                bonbon apple pie fruitcake icing biscuit apple pie jelly-o sweet roll.
+                            </p>
+                        </div>
+                    </div>
+                    <button class="btn btn-primary">Baca Selengkapnya</button>
+                </div>
+				<div class="card">
+                    <div class="card-content">
+                        <!-- Image Placeholder -->
+                        <div class="card-body">
+                            <h5 class="card-title">Be Single Minded</h5>
+                            <p class="card-text">
+                                Chocolate sesame snaps apple pie danish cupcake sweet roll jujubes tiramisu.Gummies
+                                bonbon apple pie fruitcake icing biscuit apple pie jelly-o sweet roll.
+                            </p>
+                        </div>
+                    </div>
+                    <button class="btn btn-primary">Baca Selengkapnya</button>
+                </div>
+				<div class="card">
+                    <div class="card-content">
+                        <!-- Image Placeholder -->
+                        <div class="card-body">
+                            <h5 class="card-title">Be Single Minded</h5>
+                            <p class="card-text">
+                                Chocolate sesame snaps apple pie danish cupcake sweet roll jujubes tiramisu.Gummies
+                                bonbon apple pie fruitcake icing biscuit apple pie jelly-o sweet roll.
+                            </p>
+                        </div>
+                    </div>
+                    <button class="btn btn-primary">Baca Selengkapnya</button>
+                </div>
+			</div>
+			<div class="col-xl-4 col-md-6 col-sm-12">
+                <div class="card">
+                    <div class="card-content">
+                        <!-- Image Placeholder -->
+                        <div class="card-body">
+                            <h5 class="card-title">Be Single Minded</h5>
+                            <p class="card-text">
+                                Chocolate sesame snaps apple pie danish cupcake sweet roll jujubes tiramisu.Gummies
+                                bonbon apple pie fruitcake icing biscuit apple pie jelly-o sweet roll.
+                            </p>
+                        </div>
+                    </div>
+                    <button class="btn btn-primary">Baca Selengkapnya</button>
+                </div>
+				<div class="card">
+                    <div class="card-content">
+                        <!-- Image Placeholder -->
+                        <div class="card-body">
+                            <h5 class="card-title">Be Single Minded</h5>
+                            <p class="card-text">
+                                Chocolate sesame snaps apple pie danish cupcake sweet roll jujubes tiramisu.Gummies
+                                bonbon apple pie fruitcake icing biscuit apple pie jelly-o sweet roll.
+                            </p>
+                        </div>
+                    </div>
+                    <button class="btn btn-primary">Baca Selengkapnya</button>
+                </div>
+				<div class="card">
+                    <div class="card-content">
+                        <!-- Image Placeholder -->
+                        <div class="card-body">
+                            <h5 class="card-title">Be Single Minded</h5>
+                            <p class="card-text">
+                                Chocolate sesame snaps apple pie danish cupcake sweet roll jujubes tiramisu.Gummies
+                                bonbon apple pie fruitcake icing biscuit apple pie jelly-o sweet roll.
+                            </p>
+                        </div>
+                    </div>
+                    <button class="btn btn-primary">Baca Selengkapnya</button>
+                </div>
+			</div>
+			<div class="col-xl-4 col-md-6 col-sm-12">
+                <div class="card">
+                    <div class="card-content">
+                        <!-- Image Placeholder -->
+                        <div class="card-body">
+                            <h5 class="card-title">Be Single Minded</h5>
+                            <p class="card-text">
+                                Chocolate sesame snaps apple pie danish cupcake sweet roll jujubes tiramisu.Gummies
+                                bonbon apple pie fruitcake icing biscuit apple pie jelly-o sweet roll.
+                            </p>
+                        </div>
+                    </div>
+                    <button class="btn btn-primary">Baca Selengkapnya</button>
+                </div>
+				<div class="card">
+                    <div class="card-content">
+                        <!-- Image Placeholder -->
+                        <div class="card-body">
+                            <h5 class="card-title">Be Single Minded</h5>
+                            <p class="card-text">
+                                Chocolate sesame snaps apple pie danish cupcake sweet roll jujubes tiramisu.Gummies
+                                bonbon apple pie fruitcake icing biscuit apple pie jelly-o sweet roll.
+                            </p>
+                        </div>
+                    </div>
+                    <button class="btn btn-primary">Baca Selengkapnya</button>
+                </div>
+				<div class="card">
+                    <div class="card-content">
+                        <!-- Image Placeholder -->
+                        <div class="card-body">
+                            <h5 class="card-title">Be Single Minded</h5>
+                            <p class="card-text">
+                                Chocolate sesame snaps apple pie danish cupcake sweet roll jujubes tiramisu.Gummies
+                                bonbon apple pie fruitcake icing biscuit apple pie jelly-o sweet roll.
+                            </p>
+                        </div>
+                    </div>
+                    <button class="btn btn-primary">Baca Selengkapnya</button>
+                </div>
+			</div>
+		</div>
+	</section>
