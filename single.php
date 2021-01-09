a3e3e09fa7a7c35c15864cbeb64b073b774a8ca4
Wahyu Amirulloh <amirullohw@gmail.com>
2021-01-10 05:18:09 +0700

    single develop product display

--- single.php
+++ single.php
@@ -1,6 +1,35 @@
 <?php get_header(); ?>
 <div class="col-xl-8 col-md-10 col-sm-12 product-description">
     <div class="card">
-        <?php the_post_thumbnail(); ?>
+        <div class="row">
+            <div class="col-xl-6">
+                <div class="product-image">
+                    <?php the_post_thumbnail(); ?>
+                </div>
+            </div>
+            <div class="col-xl-6">
+                <div class="product-headesc">
+                    <h2 class="title"><?php the_title(); ?></h2>
+                    <h2 class="price">
+                    <?php 
+                    if(get_field('Price')){
+                        echo 'Rp' . number_format(get_field('Price'));
+                    } else {
+                        'Free';
+                    }
+                    ?>
+                    </h2>
+                    <hr>
+                    <div class="contact">
+                        <div class="whatsapp">
+                            <a class="btn btn-outline-success">
+                                <img src="<?php echo get_template_directory_uri() ?>/assets/logo/whatsapp.png" alt="WhatsApp Icon">
+                                <span>Beli Sekarang</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
